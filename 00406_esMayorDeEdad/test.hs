57d851de9427b8ab779b662e9be0263decd8f78d
mmatos87@gmail.com <mmatos87@gmail.com>
2016-05-25 19:30:15 -0400

    Mumuki Export on 2016-05-25 19:30:15 -0400

--- 00406_esMayorDeEdad/test.hs
+++ 00406_esMayorDeEdad/test.hs
@@ -4,3 +4,6 @@ describe "esMayorDeEdad" $ do
 	
   it "esMayorDeEdad (\"juan\",15) == no es mayor de edad" $ do
     esMayorDeEdad ("juan",15) `shouldBe` False
+
+  it "esMayorDeEdad (\"juan\",18) == no es mayor de edad" $ do
+    esMayorDeEdad ("juan",18) `shouldBe` False
