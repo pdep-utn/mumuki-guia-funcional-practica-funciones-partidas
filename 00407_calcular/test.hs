7ca6a2dbe9b3221bcbedbc8d3eef4f1d887be249
luciana.benotti@gmail.com <luciana.benotti@gmail.com>
2018-09-04 10:07:26 -0400

    Mumuki Export on 2018-09-04 10:07:26 -0400

--- 00407_calcular/test.hs
+++ 00407_calcular/test.hs
@@ -1,6 +1,6 @@
-describe "calcular" $ do
+describe "calcular" $ do 
   it "calcular (7,8) == (7,9)" $ do
-    calcular (7,8) `shouldBe` (7,9)
+    calcular (7,8) `shouldBe` (7,9) 
 	
   it "calcular (2,3) == (4,4)" $ do
     calcular (2,3) `shouldBe` (4,4)
